ba2d75ada31ab93b6c4128147f0cf8c31f15396b
sarthak-mittal <sarthakmittal20@gmail.com>
2020-10-03 00:17:46 +0530

    add feature to redirect to Google Maps for Bank Address

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -52,4 +52,9 @@ handleKeyUp(event){
     }
 }
 
+redirectToGoogleMaps(){
+  let url = "https://www.google.com/maps/search/?api=1&query=" + 
+              this.bankDetails.BANK +', '+ this.bankDetails.ADDRESS 
+  window.open(url, "_blank");
+}
 }
